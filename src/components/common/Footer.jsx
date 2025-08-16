29d19a266e1d604851f67146c3a15b2008e0f56a
Mahedul-Karim <karimrupo21@gmail.com>
2025-08-16 16:20:09 +0600

    footer dummy links removed

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -2,13 +2,12 @@ import React from "react";
 import Logo from "./Logo";
 import { Instagram, Twitter, Facebook, Linkedin, Github } from "lucide-react";
 import { Link } from "react-router";
+import { NAV_LINKS } from "@/lib/data";
 
 const Footer = () => {
   return (
-    <footer
-      className={`pt-8 md:pt-16  bg-background`}
-    >
-      <section className="grid sm:grid-cols-2 lg:grid-cols-4 gap-4 l-container">
+    <footer className={`pt-8 md:pt-16  bg-background`}>
+      <section className="grid sm:grid-cols-2 lg:grid-cols-3 gap-4 l-container">
         <div>
           <Logo />
           <p className="text-sm leading-[1.5] text-muted/80 my-2 max-w-[250px]">
@@ -19,65 +18,53 @@ const Footer = () => {
         </div>
         <div>
           <h3 className="text-dark font-semibold mb-4">Company</h3>
-          <ul className="space-y-4">
-            <li>
-              <Link to="/" className="text-muted/80 text-sm">
-                About
-              </Link>
-            </li>
-            <li>
-              <Link to="/" className="text-muted/80 text-sm">
-                Contact
-              </Link>
-            </li>
-            <li>
-              <Link to="/" className="text-muted/80 text-sm">
-                Blog
-              </Link>
-            </li>
-            <li>
-              <Link to="/" className="text-muted/80 text-sm">
-                FAQ
-              </Link>
-            </li>
+          <ul className="flex flex-col gap-4">
+            {NAV_LINKS?.map((nav, i) => (
+              <li key={i}>
+                <Link to={nav.to} className="text-muted/80 text-sm">
+                  {nav.label}
+                </Link>
+              </li>
+            ))}
           </ul>
         </div>
-        <div>
-          <h3 className="text-dark font-semibold mb-4">Get Help</h3>
-          <ul className="space-y-4">
-            <li>
-              <Link to="/" className="text-muted/80 text-sm">
-                Forum
-              </Link>
-            </li>
-            <li>
-              <Link to="/" className="text-muted/80 text-sm">
-                Contact
-              </Link>
-            </li>
-            <li>
-              <Link to="/" className="text-muted/80 text-sm">
-                Community
-              </Link>
-            </li>
-          </ul>
-        </div>
+
         <div>
           <h3 className="text-dark font-semibold mb-4">Contact Us</h3>
           <div className="flex space-x-4">
-            <Link href="/" className="text-muted/80">
+            <Link
+              to="https://www.facebook.com"
+              target="_blank"
+              className="text-muted/80"
+            >
               <Facebook className="h-6 w-6" />
             </Link>
-            <Link href="#" className="text-muted/80">
+            <Link
+              to="https://instagram.com"
+              target="_blank"
+              className="text-muted/80"
+            >
               <Instagram className="h-6 w-6" />
             </Link>
-            <Link href="#" className="text-muted/80">
+            <Link
+              to="https://www.x.com"
+              target="_blank"
+              className="text-muted/80"
+            >
               <Twitter className="h-6 w-6" />
             </Link>
-            <Link href="#" className="text-muted/80">
+            <Link
+              to="https://www.linkedin.com"
+              target="_blank"
+              className="text-muted/80"
+            >
               <Linkedin className="h-6 w-6" />
             </Link>
-            <Link href="#" className="text-muted/80">
+            <Link
+              to="https://www.github.com"
+              target="_blank"
+              className="text-muted/80"
+            >
               <Github className="h-6 w-6" />
             </Link>
           </div>
